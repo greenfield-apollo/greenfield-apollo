cca1f9177077f4a5c68b49f3c482eb8abdb56c48
Erik Hsiao <ErikMHsiao@gmail.com>
2015-09-29 11:07:05 -0700

    implement GET /api/records/<habit id>

--- server/controllers/records.js
+++ server/controllers/records.js
@@ -2,6 +2,20 @@
 var Record = require('../models/record');
 
 module.exports = {
+  getRecords: function(req, res, next) {
+    var checkinDates = [];
+
+    Record.find({habitId: req.params.id}, function(err, records) {
+      if (err) return next(err);
+
+      records.forEach(function(record) {
+        checkinDates.push(record.checkinDate);
+      });
+
+      res.json({records: checkinDates});
+    });
+  },
+
   addRecord: function(req, res, next) {
     var newRecord = new Record({
       habitId: req.params.id,
