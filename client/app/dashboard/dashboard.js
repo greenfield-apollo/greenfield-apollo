f7b305d75df7facfc413bb0c89dfa9b25f3ea18e
David Churchill <dchurchill92@gmail.com>
2015-09-29 20:38:25 -0500

    add current streak bar graph, remove karma-firefox-launcher dependency

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -3,17 +3,26 @@ angular.module('app.dashboard', [])
 .controller('DashboardController', ['$rootScope', '$scope', 'Habits',
   function($rootScope, $scope, Habits) {
     $rootScope.showNav = true;
-    $scope.habits = [];
+    // Placeholder streak data
+    $scope.sampleStreaks = [
+      {habitName: 'Submit a Pull Request', streak: 15},
+      {habitName: 'Complete a Pomodoro', streak: 10},
+      {habitName: 'Workout', streak: 8}
+    ];
 
     $scope.getHabits = function() {
       Habits.getHabits()
         .then(function(habits) {
           $scope.habits = habits;
+          $scope.habitStreaks = $scope.habits.filter(function (habit) {
+            return habit.streak > 0;
+          });
+          console.log($scope.habits);
         })
         .catch(function(error) {
           console.error(error);
         });
-    };
+    }
 
     $scope.getHabits();
   }
---
+++ client/app/directives/directives.js
@@ -0,0 +1,94 @@
+angular.module('app.directives', [])
+
+.directive('d3Bars', ['$window', '$timeout',
+  function ($window, $timeout) {
+    return {
+      restrict: 'EA',
+      scope: {
+        data: '=', // bi-directional data-binding
+        label: '@',
+        onClick: '&'
+      },
+      link: function(scope, ele, attrs) {
+        scope.data.forEach(function() {})
+        var renderTimeout;
+        var margin = parseInt(attrs.margin) || 20,
+            barHeight = parseInt(attrs.barHeight) || 20,
+            barPadding = parseInt(attrs.barPadding) || 5;
+
+        var svg = d3.select(ele[0])
+          .append('svg')
+          .style('width', '100%');
+
+        $window.onresize = function() {
+          scope.$apply();
+        };
+
+        scope.$watch(function() {
+          return angular.element($window)[0].innerWidth;
+        }, function() {
+          scope.render(scope.data);
+        });
+
+        scope.$watch('data', function(newData) {
+          scope.render(newData);
+        }, true);
+
+        scope.render = function(data) {
+          svg.selectAll('*').remove();
+
+          if (!data) return;
+          if (renderTimeout) clearTimeout(renderTimeout);
+
+          renderTimeout = $timeout(function() {
+            var width = d3.select(ele[0])[0][0].offsetWidth - margin,
+                height = scope.data.length * (barHeight + barPadding),
+                color = d3.scale.category10(),
+                xScale = d3.scale.linear()
+                  .domain([0, d3.max(data, function(d) {
+                    return d.streak;
+                  })])
+                  .range([0, width]);
+
+            svg.attr('height', height);
+
+            svg.selectAll('rect')
+              .data(data)
+              .enter()
+                .append('rect')
+                .on('click', function(d,i) {
+                  return scope.onClick({item: d});
+                })
+                .attr('height', barHeight)
+                .attr('width', 140)
+                .attr('x', Math.round(margin/2))
+                .attr('y', function(d,i) {
+                  return i * (barHeight + barPadding);
+                })
+                .attr('fill', function(d) {
+                  return color(d.streak);
+                })
+                .transition()
+                  .duration(1000)
+                  .attr('width', function(d) {
+                    return xScale(d.streak);
+                  });
+            svg.selectAll('text')
+              .data(data)
+              .enter()
+                .append('text')
+                .attr('fill', '#fff')
+                .attr('y', function(d,i) {
+                  return i * (barHeight + barPadding) + 15;
+                })
+                .attr('x', 15)
+                .text(function(d) {
+                  return d.habitName + " (streak: " + d.streak + ")";
+                });
+          }, 200);
+        };
+
+      }
+    }
+  }
+])
