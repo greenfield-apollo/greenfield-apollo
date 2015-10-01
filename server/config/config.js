114011a7a02edb9a2ce03c84d879dcc3b9dc16c6
Erik Hsiao <ErikMHsiao@gmail.com>
2015-10-01 16:57:07 -0700

    test env vars

--- server/config/config.js
+++ server/config/config.js
@@ -6,6 +6,6 @@ catch (err) {}
 module.exports = {
   port: 8080,
   localdb: 'mongodb://127.0.0.1:27017/habitdb',
-  tokenSecret: env.tokenSecret || secret.tokenSecret,
-  googleSecret: env.googleSecret || secret.googleSecret
+  tokenSecret: process.env.tokenSecret || secret.tokenSecret,
+  googleSecret: process.env.googleSecret || secret.googleSecret
 };
