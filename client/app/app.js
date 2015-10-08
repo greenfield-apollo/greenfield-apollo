6f61ad90b7bee986cdcd37141e3cfbc8d760c773
Mike Hsieh <mythsieh@gmail.com>
2015-10-08 14:42:53 -0700

    Added misc. console logs

--- client/app/app.js
+++ client/app/app.js
@@ -82,21 +82,14 @@ angular.module('app', [
         $location.path('/signin');
       }
     });
-
-
     console.log('conflict testing');
-
     console.log("something to be taken out later in app.js");
-
-
     var timer;
     var eventScheduler = function() {
       console.log('more conflict testing');
       console.log('eventScheduler start');
       console.log('test1000000000');
-
       console.log('5billion13984798');
-
       Habits.getHabits()
         .then(function (habits) {
           events = Events.getEventQueue(habits);
